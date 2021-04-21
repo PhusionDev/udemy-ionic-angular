f8f88d940161e3e4e0f1c9969fa203576f2f6b86
Jared Hodgkins <phusiondev@gmail.com>
2021-04-21 12:16:29 -0400

    180. Configuring the Date Controls

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -11,21 +11,27 @@ export class PlacesService {
       'Manhattan Mansion',
       'In the heart of New York City',
       'https://leadingestates.com/wp-content/uploads/2015/11/184-10-manhattan-01.jpg',
-      149.99
+      149.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'p2',
       'L\'Amour Toujours',
       'A romantic place in Paris',
       'https://www.iata.org/contentassets/4b8ec35d55dd4596a1ee21a75aaab835/paris-330x200.jpg?w=330&h=200&mode=crop&scale=both&v=20190829085556',
-      189.99
+      189.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'p3',
       'The Foggy Palace',
       'Not your average city trip!',
       'https://images.fineartamerica.com/images-medium-large-5/foggy-palace-5-sfphotostore-.jpg',
-      99.99
+      99.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     )
   ];
 
