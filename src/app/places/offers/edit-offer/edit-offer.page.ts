158b9713b59f1b9699e84264585f0008fbd4ca2b
Jared Hodgkins <phusiondev@gmail.com>
2021-04-22 13:20:09 -0400

    191. Updating Places

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -2,8 +2,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Place } from './../../place.model';
 import { PlacesService } from './../../places.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LoadingController, NavController } from '@ionic/angular';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -19,7 +19,9 @@ export class EditOfferPage implements OnInit, OnDestroy {
   constructor(
     private route: ActivatedRoute,
     private placesService: PlacesService,
-    private navCtrl: NavController
+    private navCtrl: NavController,
+    private router: Router,
+    private loadingCtrl: LoadingController
   ) { }
 
   ngOnInit() {
@@ -49,10 +51,24 @@ export class EditOfferPage implements OnInit, OnDestroy {
       this.placeSub.unsubscribe();
     }
   }
-  onEditOffer() {
+
+  onUpdateOffer() {
     if (!this.form.valid) {
       return;
     }
-    console.log(this.form);
+    this.loadingCtrl.create(
+      { message: 'Updating place...' }
+    ).then(loadingEl => {
+      loadingEl.present();
+      this.placesService.updatePlace(
+        this.place.id,
+        this.form.value.title,
+        this.form.value.description
+      ).subscribe(() => {
+        loadingEl.dismiss();
+        this.form.reset();
+        this.router.navigate(['/places/tabs/offers']);
+      });
+    });
   }
 }
